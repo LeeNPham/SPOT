29211fdc3862aeab71401371396541d69ca3be42
Lee Pham <leenpham@gmail.com>
2023-08-01 19:02:18 -0700

    updated from axious fetch calls to generic fetch calls through the frontend

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -3,59 +3,81 @@
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import { PUBLIC_CLIENT_ID, PUBLIC_CLIENT_SECRET } from '$env/static/public';
 	import { onMount } from 'svelte';
-	import axios from 'axios';
-
-	function get_token() {
+	let songs = [];
+	async function get_token() {
 		let auth_string = PUBLIC_CLIENT_ID + ':' + PUBLIC_CLIENT_SECRET;
-		let auth_bytes = `b'${auth_string}'`;
-		const auth_base64 = btoa(auth_bytes); //might need to change this to auth_string?
-		console.log(auth_base64);
+		const auth_base64 = btoa(auth_string);
 		const url = 'https://accounts.spotify.com/api/token';
 		const headers = {
 			Authorization: 'Basic ' + auth_base64,
 			'Content-Type': 'application/x-www-form-urlencoded'
 		};
 		const data = new URLSearchParams({ grant_type: 'client_credentials' });
-		return axios
-			.post(url, data, { headers })
-			.then(async (response) => {
-				const token = response.data.access_token;
-				return token;
-			})
-			.catch((error) => {
-				console.error('Error fetching token:', error);
-				return null;
-			});
+		const retrievedToken = await fetch(url, {
+			method: 'POST',
+			headers: headers,
+			body: data
+		});
+		if (retrievedToken.ok) {
+			const res1 = await retrievedToken.json();
+			return res1['access_token'];
+		} else {
+			console.log("get token didn't work");
+			return null;
+		}
 	}
 
-	function search_for_artist(auth_header, artist_name) {
+	async function search_for_artist(auth_header, artist_name) {
 		let url = 'https://api.spotify.com/v1/search';
 		let headers = auth_header;
-		let query = `?q=${artist_name}&type=artist&limit=1`;
+		let query = `?q=${artist_name}&type=artist&limit=2`;
 		let query_url = url + query;
+		const response = await fetch(query_url, {
+			method: 'GET',
+			headers: headers
+		});
+		if (response.ok) {
+			let res2 = await response.json();
+			return {
+				artist_name: res2.artists.items[0].name,
+				artist_id: res2.artists.items[0].id
+			};
+		} else {
+			console.log("the search for artist call didn't work");
+			return null;
+		}
+	}
 
-		return axios
-			.get(query_url, { headers })
-			.then(async (response) => {
-				const artist_responsefromsearch = response.data;
-				console.log(artist_responsefromsearch);
-			})
-			.catch((error) => {
-				console.error('No Artist with this name exist', error);
-				return null;
-			});
+	async function get_songs_by_artist(auth_header, artist_id) {
+		const url = `https://api.spotify.com/v1/artists/${artist_id}/top-tracks?country=US`;
+		const headers = auth_header;
+		// result = get(url, headers=headers)
+		const result = await fetch(url, {
+			method: 'GET',
+			headers: headers
+		});
+		const res3 = await result.json();
+		// console.log(res3);
+		return res3.tracks;
 	}
 
 	onMount(async () => {
-		console.log(PUBLIC_CLIENT_ID);
-		console.log(PUBLIC_CLIENT_SECRET);
+		// console.log(PUBLIC_CLIENT_ID);
+		// console.log(PUBLIC_CLIENT_SECRET);
 		let token = await get_token();
+		// console.log(token);
 		const auth_header = { Authorization: 'Bearer ' + token };
+		// console.log({ auth_header });
 		let artist_res = await search_for_artist(auth_header, 'ACDC');
 		console.log(artist_res);
+
+		songs = await get_songs_by_artist(auth_header, artist_res.artist_id);
+		console.log(songs);
 		//try to grab a list of songs to generally display such as top tracks or playlists?
 		//refer to main.py to convert get call into a fetch to be used in onmount
 		//create a stores value for your consistently used access token
+
+		// try to grab a song too?
 	});
 </script>
 
@@ -80,9 +102,12 @@
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
 
-	<div>Hello world</div>
-	<div>This is where I will display music information, and hopefully play a song</div>
-	<div>song name</div>
+	<div class="text-lg text-black">song names (top ten)</div>
+	<div class="flex flex-col justify-start">
+		{#each songs as song, i}
+			<div>{i + 1}. {song.name}</div>
+		{/each}
+	</div>
 </section>
 
 <style>
