ad642d554ae46b2721ad42049eedbe7d7c2c5602
Lee Pham <leenpham@gmail.com>
2023-08-03 20:45:06 -0700

    Updated backend because no longer making calls from the spot api in the backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,78 +1,165 @@
-from dotenv import load_dotenv  # allows for us to get .env values
-import os  # operating system
-import base64  # type of encoding library
-import json
-from requests import post, get  # allows us to send a post request
+from database import (
+    fetch_all_users,
+    fetch_one_user,
+    create_user,
+    update_user,
+    remove_user,
+    fetch_one_user_by_username,
+)
+
+from utils import (
+    get_current_active_user,
+    authenticate_user,
+    create_access_token,
+    get_password_hash,
+)
+from datetime import timedelta
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    status,
+    Body,
+    Depends,
+    Response,
+)
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
+
+
+from model import User, UserInDb, UpdateUserModel, Token, TokenData
+import os
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-# print(client_id, client_secret)
-# authorization and authentication works differently depending on the type of api I'm using
-# since we're using a cli script, we're using a client credentials flow
-# and not a users credentials workflow
-# we will use base 64 encoding to retrieve and concat our token response
+
+access_token_expire_minutes = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-# gets access token after logging in
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    # notice: headers= header information, data= body of my request
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
+# Tokens
+@app.post("/token", response_model=Token, tags=["Tokens"])
+async def login_for_access_token(
+    response: Response, form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user = await authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=access_token_expire_minutes)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
-# gets auth header for calls after being authenticated(logged in)
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
+# Account Verification #Get Personal Account Details
+@app.get("/accounts/profile", response_model=User, tags=["User Authentication"])
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
 
 
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"
-    # query = f"q={artist_name}&type=artist,track&limit=1"
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-    print(json_result)
-    if len(json_result) == 0:
-        print("No artists with this name exists...")
-        return None
-    return json_result[0]
+@app.get("/accounts/profile/items", tags=["User Authentication"])
+async def read_own_items(
+    response: Response, current_user: User = Depends(get_current_active_user)
+):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return {"todos": current_user.todos, "categories": current_user.categories}
 
 
-token = get_token()
-# notice that different tokens are provided everytime we invoke this function
-print(token)
-result = search_for_artist(token, "ACDC")
-print(result["name"])
-artist_id = result["id"]
-print(artist_id)
-# notice: This will return a stringified json structure back to us,
-# we want to grab the artist value and the items which is our results if there's at least one of them
+# User CRUD
+@app.get("/api/user", tags=["CRUD Users"])
+async def get_users():
+    response = await fetch_all_users()
+    return response
 
 
-def get_songs_by_artist(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
-    return json_result
+@app.post(
+    "/api/user",
+    response_description="Add a new user",
+    response_model=User,
+    tags=["CRUD Users"],
+)
+async def post_user(user: UserInDb = Body(...)):
+    existing_user = await fetch_one_user_by_username(user.username)
+    if existing_user:
+        raise HTTPException(400, "Username already exists")
+    else:
+        user.username = user.username.lower()
+        user.hashed_password = get_password_hash(user.hashed_password)
+        user = jsonable_encoder(user)
+        response = await create_user(user)
+        if response:
+            return response
+        raise HTTPException(400, "Something went wrong / Bad Request")
 
 
-songs = get_songs_by_artist(token, artist_id)
+@app.get(
+    "/api/user/{id}",
+    response_description="Get a single user",
+    response_model=User,
+    tags=["CRUD Users"],
+)
+async def get_user_by_id(id: str):
+    response = await fetch_one_user(id)
+    if response:
+        return response
+    raise HTTPException(404, f"ID {id} not found")
 
-for idx, song in enumerate(songs):
-    print(f"{idx+1}. {song['name']}")
+
+@app.get(
+    "/api/user/username/{username}",
+    response_description="Get a single user by username",
+    response_model=UserInDb,
+    tags=["CRUD Users"],
+)
+async def get_user_by_username(username: str):
+    response = await fetch_one_user_by_username(username)
+    if response:
+        return response
+    raise HTTPException(404, f"{username} not found")
+
+
+@app.put(
+    "/api/user/{id}",
+    response_description="Update a user",
+    response_model=User,
+    tags=["CRUD Users"],
+)
+async def put_user(id: str, user: UpdateUserModel = Body(...)):
+    user = {k: v for k, v in user.dict().items() if v is not None}
+    user["hashed_password"] = get_password_hash(user["hashed_password"])
+    if len(user) >= 1:
+        response = await update_user(id, user)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no user account with this {id}")
+
+
+@app.delete("/api/user/{id}", response_description="Delete a user", tags=["CRUD Users"])
+async def delete_user(id: str):
+    response = await remove_user(id)
+    if response:
+        return "Successfully deleted user item"
+    raise HTTPException(404, f"There is no TODO item with this id:{id}")
+
+
+# Users End
--- Frontend/src/routes/+page.js
+++ Frontend/src/routes/+page.js
@@ -1,3 +1,25 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
 export const prerender = true;
+import { PUBLIC_BACKEND_USERS } from '$env/static/public';
+
+export async function _handleLogin(username, password) {
+	const formData = new FormData();
+	formData.append('username', username.toLowerCase());
+	formData.append('password', password);
+
+	try {
+		const response = await fetch(`${PUBLIC_BACKEND_USERS}/token`, {
+			method: 'POST',
+			body: formData
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			const { access_token } = data;
+			document.cookie = `access_token=${access_token}; path=/;`;
+		} else {
+			throw new Error('Could not Login/obtain token');
+		}
+	} catch (error) {
+		return false;
+	}
+}
